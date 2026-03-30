402181982670dc3bbda4b397e7fc8c83eb45ce87
rtrapannone <148770659+rtrapannone@users.noreply.github.com>
2026-03-30 17:40:31 +0200

    Update Scope 2 calculation.py

--- Scope 2 calculation.py
+++ Scope 2 calculation.py
@@ -1,15 +1,13 @@
-def scope2(electricity,heat,ef_location,ef_market):
-    consumption=electricity+heat
-    location_based=consumption*ef_location
-    market_based=consumption*ef_market
-    print("Location-based scope 2 emissions: " + str(location_based))
-    print("Market-based scope 2 emissions: " + str(market_based))
+def scope2(electricity,ef_location,ef_market):
+    location_based=electricity*ef_location/1000
+    market_based=electricity*ef_market/1000
+    print("Location-based scope 2 electricity emissions: " + str(location_based) + " tCO2eq")
+    print("Market-based scope 2 electricity emissions: " + str(market_based) + " tCO2eq")
 
-## For running the function call the function by inserting the electricity and heat consumption expressed in kWh during the reporting year 
+## For running the function call the function by inserting the electricity consumption expressed in kWh during the reporting year 
 ## and the two emission factors expressed in kgCO2eq/kWh
-## It returns the scope2 CO2eq expressed in kg
-## Example: scope2(5000,2000,0.3,0.2) -> Returns: Location-based scope 2 emissions: 2100.0 kgCO2eq; Market-based scope 2 emissions: 4000.0 kgCO2eq
-
+## It returns the scope2 CO2eq realted to electricity expressed in tCO2eq
+## Example: scope2(5000,0.3,0.2) -> Returns: Location-based scope 2 electricity emissions: 1.5 tCO2eq; Market-based scope 2 electricity emissions: 1.0 tC
 
 
 
